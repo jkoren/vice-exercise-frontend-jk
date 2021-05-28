13875ecbc2d2571c4315552ce3f1ac481be389e9
jkoren <j.korenstein@gmail.com>
2021-05-27 23:27:39 -0400

    nav bar displays

---
+++ app/SmallShow.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Grid from '@material-ui/core/Grid';
+
+export default function SmallShow(props) {
+  const show = props.show
+  const product_image_url = show.product_image_url
+  const border = props.selected ? 5 : 1
+
+  return (
+    <Grid item>
+      <img src={product_image_url} height="80" border={border} />
+    </Grid>
+  )
+}
--- app/index.js
+++ app/index.js
@@ -2,12 +2,22 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./application.css');
 import Main from './Main.js'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const parsedUrl = new URL(window.location.href);
+  const showId = parsedUrl.searchParams.get("id")
+
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Main 
+            showId={showId}
+          />
+        </Route> 
+      </Switch>
+    </Router>
   )
 }
 
