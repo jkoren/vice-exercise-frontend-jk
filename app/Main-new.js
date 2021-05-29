f42b883f91eb17971d275685f0d268a9b0c55caa
jkoren <j.korenstein@gmail.com>
2021-05-28 22:04:15 -0400

    completed query

--- app/Main-new.js
+++ app/Main-new.js
@@ -10,7 +10,7 @@ import SmallShow from './SmallShow';
 export const Main = (props) => {
   const [shows, setShows] = useState([])
   const [selectedShow, setSelectedShow] = useState(0)
-
+  
   const fetchShows = () => {
     const apiURL = `http://localhost:3000/shows`;
     fetch(apiURL, 
@@ -26,21 +26,24 @@ export const Main = (props) => {
     })
     .then((shows)=> {
       setShows(shows)
-      const urlShowId = shows.map(object => object.id).indexOf(props.showId)
-      setSelectedShow(urlShowId)
     })
     .catch((error) => console.error(`Error in Main.js fetch (GET):fetchShows ${error.message}`))
   }
-
+  
   useEffect(() => {
     fetchShows()
   }, [])
-
+  
   const showsFetched = (shows.length > 0)
   const show = shows[selectedShow]
 
+  const queryShowIndex = shows.map(object => object.id).indexOf(props.showId)   // the index of the show passed via query in URL
+  const resolvedIndex = queryShowIndex === -1 ? 0 : queryShowIndex
+  console.log("resolvedIndex")
+  console.log(resolvedIndex)
+  
   const showtile = !showsFetched ? "" :
-    <Show
+  <Show
       show={show}
       selectedShow={selectedShow}
       setSelectedShow={setSelectedShow}
@@ -53,20 +56,29 @@ export const Main = (props) => {
   const highEnd = [numShows - 2, numShows - 1].includes(selectedShow)
   const startShow = lowEnd ? 0 : (highEnd ? numShows - 5 : selectedShow - 2)
   const endShow = lowEnd ? 4 : (highEnd ? numShows - 1 : selectedShow + 2)
-
+  
   const showTiles = !showsFetched ? "" :
     shows.map((show, index) =>
       (index >= startShow && index <= endShow) ?
-      <SmallShow 
-        key={show.id} 
-        show={show} 
-        index={index}
-        selected={index==selectedShow}
-        setSelectedShow={setSelectedShow}
-      />
-      :
-      ""
+        <SmallShow 
+          key={show.id} 
+          show={show} 
+          index={index}
+          selected={index==selectedShow}
+          setSelectedShow={setSelectedShow}
+        />
+        :
+        ""
     )
+  
+  const thumbnails =         
+    <Box p={2} display="flex" justifyContent="center">
+      <Grid container spacing={1} justify="center" >
+        {showTiles}
+      </Grid>
+    </Box>
+
+
 
   return (
     <div>
@@ -75,11 +87,7 @@ export const Main = (props) => {
       </header>
 
       <Hidden smDown>
-        <Box p={2} display="flex" justifyContent="center">
-          <Grid container spacing={1} justify="center" >
-            {showTiles}
-          </Grid>
-        </Box>
+        {thumbnails}
       </Hidden>
 
       <Box p={1} display="flex" justifyContent="center">
@@ -87,11 +95,7 @@ export const Main = (props) => {
       </Box>
 
       <Hidden mdUp>
-        <Box p={2} display="flex" justifyContent="center">
-          <Grid container spacing={1} justify="center" >
-            {showTiles}
-          </Grid>
-        </Box>
+        {thumbnails}
       </Hidden>
 
       <footer>
