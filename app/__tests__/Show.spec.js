fa93f9b19be30816bfa6c1c28f5380152a0f171a
jkoren <j.korenstein@gmail.com>
2021-06-01 09:33:57 -0400

    added jest tests

--- app/__tests__/Show.spec.js
+++ app/__tests__/Show.spec.js
@@ -1,17 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import SmallShow from '../SmallShow'
+import Show from '../Show'
 function returnInput(inputString) {
   return inputString+"!"
 }
-
-describe("testing a jest test", () => {
-  test("it should return the input", () => {
-    const input = "React rocks"
-    const expectedOutput = "React rocks!"
-    expect(returnInput(input)).toEqual(expectedOutput)
-  })
-})
 
 it("renders without crashing",() => {
   const div = document.createElement("div")
@@ -20,13 +12,16 @@ it("renders without crashing",() => {
     id: "a1",
     product_image_url: "test.com"
   }
+  const prevShowUrl = "prevshowurl.com"
+  const nextShowUrl = "nextshowurl.com"
+
   ReactDOM.render(
-    <SmallShow
-      key={1}
+    <Show
       show={testshow}
-      index={1}
-      selected={false}
       setSelectedShow={returnInput()}
+      numShows={5}
+      prevShowUrl={prevShowUrl}
+      nextShowUrl={nextShowUrl}
     />
     , div)
 })
